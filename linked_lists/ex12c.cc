bba3c4ffac96f72c3f35fd7b7c5cfca8d1ec5231
Amiel Cohen <amielc1@gmail.com>
2024-06-09 21:15:13 +0300

    complete all EX.

--- linked_lists/ex12c.cc
+++ linked_lists/ex12c.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <new>
-#include <fstream>
 
 using namespace std;
 
@@ -10,7 +9,7 @@ struct Node {
     Node* _down = nullptr;
 };
 
-Node* creat_row_and_init_member(ifstream& input, int length_row)
+Node* creat_row_and_init_member(int length_row)
 {
     Node* head_row = nullptr;
     Node* temp = nullptr;
@@ -23,7 +22,7 @@ Node* creat_row_and_init_member(ifstream& input, int length_row)
     int val;
     for (int i = 0; i < length_row; i++)
     {
-        input >> val;
+        cin >> val;
         Node* newNode = new Node();
         newNode->_data = val;
         temp->_right = newNode;
@@ -32,17 +31,17 @@ Node* creat_row_and_init_member(ifstream& input, int length_row)
     return head_row;
 }
 
-Node* build_ds(ifstream& input)
+Node* build_ds()
 {
     Node* head_DS = nullptr;
     Node* temp = nullptr;
     int length_row;
-    input >> length_row;
+    cin >> length_row;
 
     while (length_row)
     {
         // Creating a new list(row), and inserting prisoners into the list
-        Node* new_row = creat_row_and_init_member(input, length_row);
+        Node* new_row = creat_row_and_init_member(length_row);
 
         // Threading the new row into the "DS"
         if (!head_DS)
@@ -57,7 +56,7 @@ Node* build_ds(ifstream& input)
         }
 
         // Reading an additional line length from the user
-        input >> length_row;
+        cin >> length_row;
     }
     return head_DS;
 }
@@ -83,8 +82,6 @@ void print_ds(Node* my_ds)
 
 void thread_wanted(Node* current, Node*& head_wanted, Node*& temp_head_wanted)
 {
-
-    cout << "START thread_wanted" << endl;
     if (!head_wanted)
     {
         head_wanted = current;
@@ -95,16 +92,13 @@ void thread_wanted(Node* current, Node*& head_wanted, Node*& temp_head_wanted)
         temp_head_wanted->_down = current;
         temp_head_wanted = current;
     }
-
 }
 
-// הפונקציה שמשרשרת את האיברים המבוקשים לא פועלת כראוי
 void link_wanted(Node* my_ds, int wanted)
 {
     if (!my_ds)
         return;
 
-    cout << "START link_wanted" << endl;
     Node* current = nullptr;
     Node* head_wanted = nullptr;
     Node* temp_head_wanted = nullptr;
@@ -114,8 +108,6 @@ void link_wanted(Node* my_ds, int wanted)
         current = my_ds;
         while (current)
         {
-            int TEMP = current->_data;
-
             if (current->_data == wanted)
             {
                 thread_wanted(current, head_wanted, temp_head_wanted);
@@ -129,9 +121,7 @@ void link_wanted(Node* my_ds, int wanted)
 
 void print_pointer_wanted(Node* current)
 {
-    cout << endl << "START print_pointer_wanted" << endl;
     Node* temp = current;
-   // int TEMP = current->_data; //זה מעיף את התכנית כאשר רוצים ךבדוק את הערך של NULL
     if (!current)
         return;
 
@@ -152,19 +142,14 @@ void print_wanted(Node* my_ds, int wanted)
         return;
 
     Node* current = nullptr;
-    cout << "START print_wanted" << endl;
 
     while (my_ds)
     {
         current = my_ds;
         while (current)
         {
-            cout << "NOT FOUND" << endl;
-            int TEMP = current->_data;
-
             if (current->_data == wanted)
             {
-                cout << "*****************FOUND***************" << endl;
                 print_pointer_wanted(current);
                 return;
             }
@@ -183,44 +168,25 @@ void free_ds(Node*& my_ds)
     {
         Node* tempRow = my_ds;
         my_ds = my_ds->_down;
-
         while (tempRow)
         {
             Node* current = tempRow;
             tempRow = tempRow->_right;
             delete current;
         }
-
     }
-    
     my_ds = nullptr;
 }
 
-
 int main()
 {
-    ifstream input;
-    string str = "input.txt";
-    input.open(str);
-    if (!input)
-    {
-        cerr << "The file " << str << " not opened.\n";
-        exit(1);
-    }
-
-    Node* my_ds = build_ds(input);
+    Node* my_ds = build_ds();
     print_ds(my_ds);
 
     int wanted;
-    cout << "Enter wanted       ";
-    input >> wanted;
+    cin >> wanted;
     link_wanted(my_ds, wanted);
-    cout << "SUCCESS link_wanted" << endl;
     print_wanted(my_ds, wanted);
-    cout << "SUCCESS print_wanted" << endl;
-
-    input.close();
-
     free_ds(my_ds);
 
     return EXIT_SUCCESS;
